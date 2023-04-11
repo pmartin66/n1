1499be4002af8a5c0a1100adf9f92462a95fa058
pmartin66 <115438435+pmartin66@users.noreply.github.com>
2023-04-11 13:15:31 -0400

    Update app.py

--- web_app/app.py
+++ web_app/app.py
@@ -1,57 +1,71 @@
+# importing modules
 from flask import Flask, render_template, request
 from pymysql import connections
 import os
 import random
 import argparse
-import socket
-
+import boto3
 
 app = Flask(__name__)
+
 DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
 DBPWD = os.environ.get("DBPWD") or "password"
 DATABASE = os.environ.get("DATABASE") or "employees"
-#COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
-#DBPORT = int(os.environ.get("DBPORT"))
-My_name = os.environ.get('MYNAME')
-image_url=os.environ.get('image_url')
-#if os.path.exists('/clo835/config/image_url'):
- #My_file = open("/clo835/config/image_url", "r")
- #image_url = My_file.read()
- #print("Background image url is ",image_url)
+DBPORT = int(os.environ.get("DBPORT"))
+BACK_ENV = os.environ.get("APP_bg") or "set1"
+AWS_ACCESS_KEY=os.environ.get("AWS_ACCESS_KEY")
+AWS_SECRET_KEY=os.environ.get("AWS_SECRET_KEY")
+AWS_SESSION_TOKEN=os.environ.get("AWS_SESSION_TOKEN")
+
 
 # Create a connection to the MySQL database
-###db_conn = connections.Connection(
-    #host= DBHOST,
-    #port=DBPORT,
-    #user= DBUSER,
-    #password= DBPWD, 
-    #db= DATABASE
-    
-    #)
+db_conn = connections.Connection(
+    host= DBHOST,
+    port=DBPORT,
+    user= DBUSER,
+    password= DBPWD, 
+    db= DATABASE
+)
+
+s3 = boto3.client('s3',
+aws_access_key_id = AWS_ACCESS_KEY, aws_secret_access_key= AWS_SECRET_KEY ,aws_session_token= AWS_SESSION_TOKEN ,region_name="us-east-1")
+
 output = {}
 table = 'employee';
 
 # Define the supported color codes
-#color_codes = {
-#    "red": "#e74c3c",
-#    "green": "#16a085",
-#    "blue": "#89CFF0",
-#    "blue2": "#30336b",
-#    "pink": "#f4c2c2",
-#    "darkblue": "#130f40",
-#    "lime": "#C1FF9C",
-#}
+bg_codes = {
+    "set1": "https://background-storage.s3.amazonaws.com/pexels-photo-6037011.jpeg",
+    "set2": "https://background-storage.s3.amazonaws.com/images+(1).jfif",
+    "set3": "https://background-storage.s3.amazonaws.com/images+(2).jfif",
+    "set4": "https://background-storage.s3.amazonaws.com/images.jfif",
+
+}
+
+# Create a string of supported colors
+SUPPORTED_BG = ",".join(bg_codes.keys())
 
 # Generate a random color
-#COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
+bg = random.choice(["set1", "set2", "set3", "set4"])
 
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('addemp.html', background=bg_codes[bg])
+
+@app.route("/about", methods=['GET','POST'])
+def about():
+    return render_template('about.html', background=bg_codes[bg])
     
-@app.route("/", methods=['GET','POST'])
-def main():
-   return render_template('addemp.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") )
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=81)
+@app.route("/addemp", methods=['POST'])
+def AddEmp():
+    emp_id = request.form['emp_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    primary_skill = request.form['primary_skill']
+    location = request.form['location']
+
   
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
